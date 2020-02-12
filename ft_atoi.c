9da957c9abd7d0195d1737e2f9cce56a3a5d28d4
BennyBaguette <benjamin.jacquet@live.fr>
2020-02-12 19:15:28 +0100

    initial commit

---
+++ ft_atoi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jabenjam <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 19:30:12 by jabenjam          #+#    #+#             */
+/*   Updated: 2019/11/20 14:45:02 by jabenjam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_atoi(const char *str)
+{
+	int					i;
+	size_t				result;
+	int					sign;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+		sign = str[i++] == '-' ? -1 : 1;
+	while (str[i] >= '0' && str[i] <= '9')
+		result = (result * 10) + (str[i++] - 48);
+	return (result * sign);
+}
---
+++ ft_strjoin.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jabenjam <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/08 15:53:10 by jabenjam          #+#    #+#             */
+/*   Updated: 2019/11/25 19:51:40 by jabenjam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	unsigned int	i;
+	unsigned int	j;
+	char			*new;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = ft_strlen(s1) + ft_strlen(s2);
+	j = 0;
+	if (!(new = malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		new[i + j] = s2[j];
+		j++;
+	}
+	new[i + j] = '\0';
+	return (new);
+}
