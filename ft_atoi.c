992f156172c30b1c055df4bd082ced2456249ac2
Jacquet BENJAMIN <jabenjam@e2r8p1.clusters.42paris.fr>
2021-12-29 12:28:35 +0100

    fix on atoi and atol

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,25 +6,28 @@
 /*   By: jabenjam <jabenjam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 19:30:12 by jabenjam          #+#    #+#             */
-/*   Updated: 2021/04/15 15:16:36 by jabenjam         ###   ########.fr       */
+/*   Updated: 2021/12/29 12:28:14 by jabenjam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	int					i;
-	size_t				result;
-	int					sign;
+	int		i;
+	size_t	result;
+	int		sign;
 
 	i = 0;
 	result = 0;
 	sign = 1;
-	while (ft_isspace(str[i]))
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
-	if (str[i] == '-' || str[i] == '+')
-		sign = str[i++] == '-' ? -1 : 1;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
 	while (str[i] >= '0' && str[i] <= '9')
 		result = (result * 10) + (str[i++] - 48);
 	return (result * sign);
@@ -39,10 +42,13 @@ long	ft_atol(const char *str)
 	i = 0;
 	result = 0;
 	sign = 1;
-	while (ft_isspace(str[i]))
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
-	if (str[i] == '-' || str[i] == '+')
-		sign = str[i++] == '-' ? -1 : 1;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
 	while (str[i] >= '0' && str[i] <= '9')
 		result = (result * 10) + (str[i++] - 48);
 	return (result * sign);
